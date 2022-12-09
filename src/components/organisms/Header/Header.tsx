976f630ecd364cc40aefd62f3f908eb322de3dc5
Sebastian Deme <deme.sebastian@gmail.com>
2022-12-09 20:48:26 +0200

    fix: build error

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,5 +1,4 @@
-import { Fragment } from 'react';
-import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Disclosure, Menu } from '@headlessui/react';
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
 import { CONSTANTS, useAppContext } from '../AppContextProvider/AppContextProvider.tsx';
 
